05ccc2facb64a697a86b05f1456fbc79c8b0abc1
andretomazela-dev <andre.tomazela@gmail.com>
2026-05-04 15:57:19 -0300

    Add files via upload

---
+++ curadoria-raiz/app/layout.jsx
@@ -0,0 +1,31 @@
+
+import './globals.css'
+import { Playfair_Display, Inter } from 'next/font/google'
+
+const playfair = Playfair_Display({
+  subsets: ['latin'],
+  variable: '--font-serif',
+})
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
+
+export const metadata = {
+  title: 'Curadoria Raiz',
+  description:
+    'Uma seleção mensal de vinho natural, pão artesanal e produtos de pequenos produtores.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='pt-BR'>
+      <body
+        className={`${playfair.variable} ${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
