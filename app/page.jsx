ff2c19b9c995537756ebe5b438e93f795ad3d4ae
andretomazela-dev <andre.tomazela@gmail.com>
2026-05-13 17:20:18 -0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -676,7 +676,7 @@ export default function Home() {
         alt="Curadoria Raiz"
         width={420}
         height={120}
-       className="h-auto w-[308px] sm:w-[364px]"
+       className="h-auto w-[370px] sm:w-[437px]"
       />
     </div>
 
